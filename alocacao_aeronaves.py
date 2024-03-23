57fffb8e4d6e9d541e891a5437b8c177e7895585
Sena2K <senamurilo2@gmail.com>
2024-03-23 19:13:38 -0300

    o nome da branch diz tudo.

--- alocacao_aeronaves.py
+++ alocacao_aeronaves.py
@@ -1,25 +1,5 @@
 import random
 
-import numpy as np
-from matplotlib import pyplot as plt
-
-# Definindo os dados do problema
-# rotas = [
-#     ("São Paulo (GRU)", "Rio de Janeiro (GIG)"),
-#     ("São Paulo (GRU)", "Brasília (BSB)"),
-#     ("São Paulo (GRU)", "Belo Horizonte (CNF)"),
-#     ("Rio de Janeiro (GIG)", "São Paulo (GRU)"),
-#     ("Rio de Janeiro (GIG)", "Brasília (BSB)"),
-#     ("Rio de Janeiro (GIG)", "Belo Horizonte (CNF)"),
-#     ("Brasília (BSB)", "São Paulo (GRU)"),
-#     ("Brasília (BSB)", "Rio de Janeiro (GIG)"),
-#     ("Brasília (BSB)", "Belo Horizonte (CNF)"),
-#     ("Belo Horizonte (CNF)", "São Paulo (GRU)"),
-#     ("Belo Horizonte (CNF)", "Rio de Janeiro (GIG)"),
-#     ("Belo Horizonte (CNF)", "Brasília (BSB)")
-# ]
-
-
 voos_diarios = {
     ("São Paulo (GRU)", "Rio de Janeiro (GIG)"): (1.0, 10),
     ("São Paulo (GRU)", "Brasília (BSB)"): (2.0, 6),
@@ -38,25 +18,31 @@ voos_diarios = {
 
 def gerar_individuo():
     individuo = {}
+    aviao_id = 1
     for rota in voos_diarios:
         duracao_voo, num_voos_diarios = voos_diarios[rota]
         horarios = []
         for _ in range(num_voos_diarios):
-            # embarque e desembarque
             embarque = 1.0
             desembarque = 0.5
             horario = random.randint(6 + int(embarque), 24 - int(duracao_voo) - int(desembarque))
-            horarios.append(horario)
+            horarios.append((aviao_id, horario))
+            aviao_id += 1
         individuo[rota] = horarios
     return individuo
 
-def calcular_fitness(individuo): # nao sei se faz mais sentido, to pensando sobre...
+def calcular_fitness(individuo):
+    avioes_utilizados = set()
+    for rota, horarios in individuo.items():
+        for aviao_id, _ in horarios:
+            avioes_utilizados.add(aviao_id)
+
     avioes_ocupados = set()
     for rota, horarios in individuo.items():
         duracao_voo, _ = voos_diarios[rota]
         embarque = 1.0
         desembarque = 0.5
-        for horario in horarios:
+        for _, horario in horarios:
             for hora in range(horario - int(embarque), horario + int(duracao_voo) + int(desembarque) + 1):
                 if 6 <= hora <= 24:
                     avioes_ocupados.add(hora)
@@ -64,8 +50,8 @@ def calcular_fitness(individuo): # nao sei se faz mais sentido, to pensando sobr
     aeronaves_nao_utilizadas = set(range(6, 25)) - avioes_ocupados
     penalidade = len(aeronaves_nao_utilizadas)
 
-    return len(avioes_ocupados) + penalidade
-
+    numero_avioes_necessarios = len(avioes_utilizados)
+    return numero_avioes_necessarios + penalidade
 
 
 def selecao_torneio(populacao, tamanho_torneio):
@@ -138,6 +124,7 @@ def mutacao_troca_voos(individuo):
 #     return individuo
 
 
+
 def algoritmo_genetico(tamanho_populacao, geracoes, tamanho_torneio):
     populacao = [gerar_individuo() for _ in range(tamanho_populacao)]
     for _ in range(geracoes):
@@ -146,86 +133,19 @@ def algoritmo_genetico(tamanho_populacao, geracoes, tamanho_torneio):
             pai1 = selecao_torneio(populacao, tamanho_torneio)
             pai2 = selecao_torneio(populacao, tamanho_torneio)
             filho = crossover(pai1, pai2)
-            if random.random() < 0.1:  # Chance de mutaçao
+            if random.random() < 0.1:  # Chance de mutação
                 filho = mutacao_troca_voos(filho)
             nova_populacao.append(filho)
         populacao = nova_populacao
     melhor_individuo = min(populacao, key=lambda ind: calcular_fitness(ind))
     return melhor_individuo
 
-
 melhor_solucao = algoritmo_genetico(tamanho_populacao=50, geracoes=100, tamanho_torneio=5)
 print("Melhor alocação de aviões:")
 for rota, horarios in melhor_solucao.items():
-    print(f"{rota[0]} -> {rota[1]}: {horarios}")
+    print(f"{rota[0]} -> {rota[1]}:")
+    for aviao_id, horario in horarios:
+        print(f"Avião {aviao_id}: {horario}")
 print("Fitness:", calcular_fitness(melhor_solucao))
+print("Número de aviões utilizados:", len(set(aviao_id for horarios in melhor_solucao.values() for aviao_id, _ in horarios)))
 
-
-#Isso aqui foi uma ia que fez obviamente kkkkkk, dps eu tiro so qria ver
-# def analisar_resultados(melhor_solucao):
-#   """
-#   Realiza uma análise detalhada da melhor solução do algoritmo genético.
-#
-#   Args:
-#     melhor_solucao: Dicionário que representa a melhor solução.
-#
-#   Returns:
-#     None.
-#   """
-#
-#   # Distribuição dos horários dos voos
-#   for rota, horarios in melhor_solucao.items():
-#     # Gráfico de dispersão
-#     plt.scatter(horarios, range(len(horarios)))
-#     plt.xlabel("Horário de Partida")
-#     plt.ylabel("Frequência")
-#     plt.title(f"Distribuição dos horários - {rota}")
-#     plt.show()
-#
-#     # Histograma
-#     plt.hist(horarios)
-#     plt.xlabel("Horário de Partida")
-#     plt.ylabel("Frequência")
-#     plt.title(f"Histograma dos horários - {rota}")
-#     plt.show()
-#
-#   # Sobreposição de horários
-#   matriz_sobreposicao = np.zeros((len(voos_diarios), len(voos_diarios)))
-#   for rota1, horarios1 in melhor_solucao.items():
-#     for rota2, horarios2 in melhor_solucao.items():
-#       if rota1 != rota2:
-#         for h1 in horarios1:
-#           for h2 in horarios2:
-#             if h1 == h2:
-#               matriz_sobreposicao[voos_diarios.index((rota1, rota2))] += 1
-#
-#   # Imprimir matriz de sobreposição
-#   print("Matriz de sobreposição:")
-#   print(matriz_sobreposicao)
-#
-#   # Identificar rotas com maior sobreposição
-#   rotas_sobrepostas = []
-#   for i in range(len(matriz_sobreposicao)):
-#     for j in range(len(matriz_sobreposicao[0])):
-#       if matriz_sobreposicao[i][j] > 0:
-#         rotas_sobrepostas.append((voos_diarios.keys()[i], voos_diarios.keys()[j]))
-#
-#   # Imprimir rotas com maior sobreposição
-#   print("Rotas com maior sobreposição:")
-#   print(rotas_sobrepostas)
-#
-#   # Utilização de cada aeronave
-#   avioes_utilizados = {}
-#   for rota, horarios in melhor_solucao.items():
-#     duracao_voo, num_voos_diarios = voos_diarios[rota]
-#     avioes_utilizados[rota] = num_voos_diarios * (int(duracao_voo) + 1)
-#
-#   # Gráfico de barras
-#   plt.bar(avioes_utilizados.keys(), avioes_utilizados.values())
-#   plt.xlabel("Rota")
-#   plt.ylabel("Número de Voos")
-#   plt.title("Utilização de cada aeronave")
-#   plt.show()
-#
-# # Executar a função
-# analisar_resultados(melhor_solucao)
